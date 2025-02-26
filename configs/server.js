0334e90db04205790018c42e783ba7e776758740
Informatica <informatica@alumno.local>
2025-02-26 14:05:00 -0600

    Sumo Administrador unico Creado y Operativo

--- configs/server.js
+++ configs/server.js
@@ -6,20 +6,69 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import { dbConnection } from './mongo.js';
 import { hash } from "argon2";
+import limiter from '../src/middlewares/validar-cant-peticiones.js'
+import Usuario from "../src/user/user.model.js";
+import Role from "../src/role/role.model.js";
 
 const configurarMiddlewares = (app) => {
-   
+    app.use(express.urlencoded({extended: false}));
+    app.use(cors());
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan('dev'));
+    app.use(limiter);
 }
-
 const configurarRutas = (app) =>{
 
 
 }
 
+const initializeRoles = async () => {
+    try {
+        const defaultRole = await Role.findOne({ role: "ADMIN_ROLE" });
+        if (!defaultRole) {
+            await Role.create({ role: "ADMIN_ROLE" });
+            console.log("Rol por defecto creado: ADMIN_ROLE");
+        } else {
+            console.log("Rol por defecto ya existente");
+        }
+    } catch (error) {
+        console.error("Error al inicializar el rol:", error);
+    }
+};
+
+const crearAdmin = async () => {
+    try {
+        const adminExistente = await Usuario.findOne({ role: "ADMIN_ROLE" });
+
+        if (!adminExistente) {
+            const passwordEncriptada = await hash("SumoAdmin");
+
+            const admin = new Usuario({
+                name: "Admin",
+                surname: "Admin",
+                username: "admin",
+                email: "admin@gmail.com",
+                phone: "12345",
+                password: passwordEncriptada,
+                role: "ADMIN_ROLE"
+            });
+
+            await admin.save();
+            console.log("Administrador creado exitosamente.");
+        } else {
+            console.log("El administrador ya existe.");
+        }
+    } catch (error) {
+        console.error("Error al crear el administrador:", error);
+    }
+};
+
 const conectarDB = async () => {
     try {
         await dbConnection();
         console.log("Conexion Exitosa Con La Base De Datos");
+        await initializeRoles();
     } catch (error) {
         console.log("Error Al Conectar Con La Base De Datos", error);
     }
@@ -30,6 +79,7 @@ export const iniciarServidor = async () => {
     const port = process.env.PORT || 3000;
 
     await conectarDB();
+    await crearAdmin();
     configurarMiddlewares(app);
     configurarRutas(app);
 
